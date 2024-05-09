287b8b66e297ba5a5a039160330978f5bea6d24f
Faizan Mirza <109523560+FaizanMirza711@users.noreply.github.com>
2024-05-09 23:51:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from PIL import image,imageaTk
+root = Tk()
+#gui logic here
+#geometry for gui
+root.geometry("712x260") #width and height resp
+#min size
+root.minsize(200,100)
+#max size
+root.maxsize(1200,900)
+#lable
+image= image.open("backgroung.png")
+background = imageTk.PhotoImage(image)
+heading=Label(image=background)
+heading.pack()# rule for tkinter to take into consideration
+
+
+
+root.mainloop()
